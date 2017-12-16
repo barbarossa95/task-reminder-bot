6f411430b3a8449b9557c7164e430e2aa19a63a8
v.greshilov <vgreshilov@mail.ru>
2017-12-16 16:18:18 +0600

    Bot class prototype

--- bot.js
+++ bot.js
@@ -6,6 +6,32 @@
  * Bot main file
  */
 
- function Bot() {
+ class Bot() {
+    // Bot Constuctor
+    constructor(options) {
+        let TelegramBotApi = require('node-telegram-bot-api');
 
+        // Init props
+        this.telegramBotToken = options.telegramBotToken;
+        this.port = options.port;
+        this.url = options.url;
+
+        // Init botApi
+        this.botApi = new TelegramBotApi(this.telegramBotToken, {
+            webHook: {
+                port: this.port
+            }
+        });
+
+        // Set up webhook
+        this.botApi.setWebHook(`${this.url}/bot${this.telegramBotToken}`);
+
+        //this.botApi.onText(/\/start/, this.cmdStart);
+    }
+
+    cmdStart(msg) {
+        this.botApi.sendMessage(msg.chat.id, "Greetings, dude\nI'm here to help you");
+    }
  }
+
+ module.exports = Bot;
