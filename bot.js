b4baaac2d18fe7273fc9abbb696fa5f25f384fe1
v.greshilov <vgreshilov@mail.ru>
2017-12-16 13:11:03 +0600

    Added main files Added proc file

---
+++ bot.js
@@ -0,0 +1,11 @@
+/**
+ * bot.js
+ *
+ * Created by Greshilov at 16.12.2017
+ *
+ * Bot main file
+ */
+
+ function Bot() {
+
+ }
---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * index.js
+ *
+ * Created by V.Greshilov at 16.12.2017
+ *
+ *
+ * Bot index file
+ */
+let TaskReminderBot = require('./bot'),
+    bot = new TaskReminderBot();
