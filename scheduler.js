00ae4f944093580bcd639903841043ba8aa908aa
Vladimir Greshilov <greshilov@a2design.biz>
2017-12-18 10:56:25 +0600

    Added scheluder class

---
+++ scheduler.js
@@ -0,0 +1,47 @@
+/**
+ * scheduler.js
+ *
+ * Created by V.Greshilov at 18.12.2017
+ *
+ * Class for running sendings sheduled messages to users
+ */
+
+class Sheduler {
+    // Scheduler constuctor
+    constuctor () {
+        this.mongolabUri = process.env.MONGOLAB_URI;
+        this.MongoClient = require('mongodb').MongoClient;
+    }
+
+    job () {
+        this.MongoClient.connect(this.mongolabUri, function (err, client) {
+            if (err) return;
+            console.log("Conneted to mongo db");
+            const db = client.db;
+
+            let date = new Date(),
+                description = "Test task description";
+            date = date.toJSON();
+            let task = {
+                description,
+                date
+            };
+
+            db.collection('tasks').insertOne(task, function (err, r) {
+                r.insertedCount == 1
+                ? console.log('inserted one documet')
+                : console.log(err);
+            });
+        })
+    }
+
+    start () {
+        this.timeInterval = setInterval(this.job.bind(this), 5000);
+    }
+
+    stop () {
+        clearInterval(this.timeInterval);
+    }
+}
+
+module.exports = Sheduler;
