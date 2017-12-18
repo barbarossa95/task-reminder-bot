9b3b0e126fef7e9abc098bb6853b1bbc782c52c5
v.greshilov <vgreshilov@mail.ru>
2017-12-18 22:37:50 +0600

    Updated index.js

--- index.js
+++ index.js
@@ -12,21 +12,27 @@ const port = process.env.PORT || 443;
 // Webhook url
 const url = process.env.APP_URL || 'https://glacial-shelf-62769.herokuapp.com';
 // Mongo db connection uri
-const mongoUri = process.env.APP_URL || "mongodb://heroku_q6tk00h5:kdl956cponc3b8al8fbt2kdc21@ds059546.mlab.com:59546/heroku_q6tk00h5";
-// Our bot class
-const TaskReminderBot = require('./bot');
-// Our scheluder class
-const Scheduler = require('./scheduler');
+const mongoUri = mongoDbUriprocess.env.MONGODB_URI || "mongodb://heroku_q6tk00h5:kdl956cponc3b8al8fbt2kdc21@ds059546.mlab.com:59546/heroku_q6tk00h5";
+// Mongo db name
+const dataBaseName = process.env.MONGODB_NAME || "heroku_q6tk00h5";
 
-let options = {
+const options = {
     telegramBotToken,
     url,
-    port,
-    mongoUri
+    port
 };
 
+// Our bot class
+const TaskReminderBot = require('./src/bot');
+// Our scheluder class
+const Scheduler = require('./src/scheduler');
+// Our task controller class
+const TaskController = require('./src/task-controller');
+
+// Task controller
+const taskController = new TaskController(mongoUri, dataBaseName);
 // Bot instance
-const bot       = new TaskReminderBot(options);
+const bot       = new TaskReminderBot(options, taskController);
 // Scheduler instance
-const scheduler = new Scheduler();
+const scheduler = new Scheduler(taskController, bot);
 scheduler.start();
